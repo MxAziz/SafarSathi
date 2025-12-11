8e05d02680d88d9a05182a0ccb389939dc15b799
MxAziz <hereabdulaziz@gmail.com>
2025-12-11 22:05:35 +0600

    added auth service refresh token

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,6 +3,9 @@ import { UserStatus } from "../../../generated/prisma/enums.js";
 import AppError from "../../errorHelpers/AppError.js";
 import { prisma } from "../../lib/prisma.js";
 import { createUserTokens } from "../../utils/userToken.js";
+import { generateToken, verifyToken } from "../../utils/jwt.js";
+import config from "../../config/index.js";
+import type { JwtPayload } from "jsonwebtoken";
 
 
 const login = async (payload: { email: string; password: string }) => {
@@ -50,7 +53,49 @@ const login = async (payload: { email: string; password: string }) => {
   };
 };
 
+const refreshToken = async (token: string)=> {
+  let decodedData;
+  try {
+    decodedData = verifyToken(
+      token,
+      config.JWT.REFRESH_TOKEN_SECRET
+    ) as JwtPayload;
+  } catch (error) {
+    throw new Error("You are not authorized!");
+  }
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: decodedData.email,
+      status: UserStatus.ACTIVE,
+    },
+  });
+
+  if (userData.isDeleted === true) {
+    throw new AppError(404, "Your account is deleted");
+  }
+
+  // if (userData.isVerified === false) {
+  //   throw new AppError(404, "Your account is not verified");
+  // }
+
+  const tokenPayload = {
+    email: userData.email,
+    role: userData.role,
+    id: userData.id,
+  };
+  const accessToken = generateToken(
+    tokenPayload,
+    config.JWT.ACCESS_TOKEN_SECRET,
+    config.JWT.ACCESS_TOKEN_EXPIRATION
+  );
+
+  return {
+    accessToken,
+  };
+}
+
 
 export const AuthService = {
-    login,
+  login,
+  refreshToken,
 }
