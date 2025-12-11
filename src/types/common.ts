979add504a46d2a72cbde2c6db5d921d69823c49
MxAziz <hereabdulaziz@gmail.com>
2025-12-11 12:15:28 +0600

    added some modules file

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,7 +1,12 @@
-import { UserRole } from "@prisma/client";
+// import { UserRole } from "@prisma/client";
 
 export type IJwtPayload = {
   email: string;
   role: UserRole;
   id: string;
 };
+
+enum UserRole{
+  ADMIN,
+  TRAVELER
+}
