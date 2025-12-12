34288543313d35f717939c698e20e868a4f92258
MxAziz <hereabdulaziz@gmail.com>
2025-12-12 18:28:11 +0600

    added some base routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import userRoute from "../modules/user/user.routes.js";
 import authRoute from "../modules/auth/auth.routes.js";
+import travelPlanRoute from "../modules/travelPlans/travelPlans.routes.js";
+import reviewRoute from "../modules/reviews/reviews.routes.js";
+import tripRequestRoute from "../modules/tripRequest/tripRequest.routes.js";
 
 const routes = Router();
 
 routes.use("/auth", authRoute);
 routes.use("/users", userRoute);
+routes.use("/travel-plans", travelPlanRoute);
+routes.use("/reviews", reviewRoute);
+routes.use("/trip-requests", tripRequestRoute);
 
 export default routes;
